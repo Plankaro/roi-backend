4f746cd2820d590829943f7f74b806a9851abae8
Ankijit2 <developerplankaro@gmail.com>
2025-05-08 11:39:38 +0530

    test fix

--- src/auth/guards/authguard.ts
+++ src/auth/guards/authguard.ts
@@ -29,6 +29,7 @@ export class AuthGuard implements CanActivate {
   
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
+    console.log(token)
     if (!token) {
       throw new UnauthorizedException('Authentication failed');
     }
